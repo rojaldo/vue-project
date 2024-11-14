068672cf168445f850cc169f9fbbb8de904195c1
Daniel Rojo <danielrojo@gmail.com>
2024-11-14 17:44:43 +0100

    apod component with getapod() in apodinfo component

--- src/components/apod/ApodComponent.vue
+++ src/components/apod/ApodComponent.vue
@@ -4,18 +4,10 @@
             class="container"
         >
             <div class="row align-items-md-stretch">
-                <div class="col-md-12" v-if="apod">
+                <div class="col-md-12">
                     <ApodPicker @update:currentDate="currentDate = $event" />
-                    <ApodInfo :apodInfo="apod" />
-                </div>
-                <div
-                    class="alert alert-primary"
-                    role="alert"
-                    v-else
-                >
-                    <strong>Loading from APOD</strong> 
-                </div>
-                
+                    <ApodInfo :apodDate="currentDate" />
+                </div>                
 
             </div>
             
@@ -35,30 +27,7 @@ let apod: Ref<Apod | null> = ref(null);
 
 const currentDate = ref("");
 
-watch(currentDate, (newVal, oldVal) => {
-    console.log(newVal);
-    getApod(newVal);
-});
 
-const getApod = async (dateStr?: string) => {
-
-    let url = '';
-    const baseUrl = 'https://api.nasa.gov/planetary/apod';
-    const apiKey  = 'DEMO_KEY';
-    if (dateStr !== undefined && dateStr !== null && dateStr !== '') {
-        url = `${baseUrl}?api_key=${apiKey}&date=${dateStr}`;
-    } else {
-    url = `${baseUrl}?api_key=${apiKey}`;
-    }
-
-    const response = await fetch(url);
-    const data = await response.json();
-    apod.value = new Apod(data);
-}
-
-
-
-getApod();
 </script>
 
 <style scoped>
--- src/components/apod/ApodInfo.vue
+++ src/components/apod/ApodInfo.vue
@@ -1,30 +1,31 @@
 <template>
+    <p>{{ props.apodDate }}</p>
     <div
         class="h-100 p-5 text-black bg-light border rounded-3"
     >
-        <h2>{{apodInfo.title}}</h2>
-        <h5>{{ apodInfo.date }}</h5>
+        <h2>{{apod?.title}}</h2>
+        <h5>{{ apod?.date }}</h5>
         <img
-            v-if="apodInfo.isImage"
-            :src="apodInfo.url"
-            :alt="apodInfo.title"
+            v-if="apod?.isImage"
+            :src="apod?.url"
+            :alt="apod?.title"
             class="img-fluid"
         />
         <iframe
-            v-else-if="apodInfo.isVideo"
-            :src="apodInfo.url"
+            v-else-if="apod?.isVideo"
+            :src="apod?.url"
             width="560"
             height="315"
             frameborder="0"
             allowfullscreen
         ></iframe>
         <p>
-            {{apodInfo.explanation}}
+            {{apod?.explanation}}
         </p>
         <button
             class="btn btn-outline-primary"
             type="button"
-            :href="apodInfo.hdUrl"
+            :href="apod?.hdUrl"
         >
             HD Image
         </button>
@@ -33,11 +34,46 @@
 
 <script setup lang="ts">
 
-import type { Apod } from '@/models/apod';
+import { Apod } from '@/models/apod';
+import { get } from 'vant/lib/utils';
+import { onBeforeUpdate, ref, watch, type Ref } from 'vue';
 
 const props = defineProps<{
-    apodInfo: Apod;
+    apodInfo?: Apod;
+    apodDate?: string;
 }>();
+
+onBeforeUpdate(() => {
+    //check with regex if the date is in the correct format
+    if (props.apodDate !== undefined && props.apodDate !== null && props.apodDate !== '') {
+        console.log('apodDate', props.apodDate);
+        getApod(props.apodDate);
+    }
+});
+
+const apod: Ref<Apod | null> = ref(null);
+
+const getApod = async (dateStr?: string) => {
+    if (apod.value?.date === dateStr) return;
+    let url = '';
+    const baseUrl = 'https://api.nasa.gov/planetary/apod';
+    const apiKey  = 'DEMO_KEY';
+    if (dateStr !== undefined && dateStr !== null && dateStr !== '') {
+        url = `${baseUrl}?api_key=${apiKey}&date=${dateStr}`;
+    } else {
+    url = `${baseUrl}?api_key=${apiKey}`;
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+    // check if date from api is different from the apod.date
+    console.log('data', data);
+    
+    
+    apod.value = new Apod(data);
+}
+
+getApod();
 
 </script>
 
